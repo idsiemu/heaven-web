5d0c066857e94dcbca2fc68b06f6dea7a2753caf
SMK <idsiemu@gmail.com>
2021-08-07 19:12:14 +0900

    사역 타이틀 끝

--- pages/service/ministry/title.tsx
+++ pages/service/ministry/title.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import AbstractComponent from '@components/abstract';
 import styled from 'styled-components';
 import Container from '@material-ui/core/Container';
@@ -13,26 +13,28 @@ import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
 import DatePicker from '@material-ui/lab/DatePicker';
 import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 import { HButton } from '@components/button/styled';
-import { DocumentNode, gql, useMutation, useQuery } from '@apollo/client';
+import { DocumentNode, gql, useMutation } from '@apollo/client';
 import Progress from '@components/progress';
 import { useCookie } from 'next-cookie';
 import { TOKEN } from 'src/assets/utils/ENV';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import { HSnack, ISnack } from '@components/snackbar/styled';
 import { useEffect } from 'react';
 import { NextPageContext } from 'next';
 import { CircularProgress } from '@material-ui/core';
+import { NameNode, OperationDefinitionNode } from 'graphql';
+import axiosApiInstance from 'src/axios';
+import { GQL_DOMAIN } from 'src/assets/utils/ENV';
+import GlobalStyle from '@styles/globalStyles';
 
-
-
-export const getServerSideProps = async (context:NextPageContext) => {
+export const getServerSideProps = async (context: NextPageContext) => {
     return {
-        props : {
+        props: {
             query: context?.query,
-            params : {}
+            params: {}
         }
     };
-  };
+};
 
 const TitleContainer = styled(Container)`
     && {
@@ -44,14 +46,21 @@ const TitleContainer = styled(Container)`
 `;
 
 export const GET_TITLE: DocumentNode = gql`
-    query getTitle($role: Int) {
-        getTitle(role: $role) {
-            status,
-            data,
-            token,
+    query getTitle($idx: Int) {
+        getTitle(idx: $idx) {
+            status
+            data {
+                idx
+                title
+                brief_history {
+                    when
+                    content
+                }
+            }
+            token
             errors {
-                code,
-                var,
+                code
+                var
                 text
             }
         }
@@ -59,18 +68,19 @@ export const GET_TITLE: DocumentNode = gql`
 `;
 
 export const SET_TITLE: DocumentNode = gql`
-    mutation setTitle($service:ServiceInput!) {
-        setTitle(service:$service) {
-            status,
-            token,
+    mutation setTitle($service: ServiceInput!) {
+        setTitle(service: $service) {
+            status
+            token
+            location
             errors {
-                code,
-                var,
+                code
+                var
                 text
             }
         }
     }
-`
+`;
 
 interface IBrief {
     open: boolean;
@@ -79,26 +89,24 @@ interface IBrief {
     content: string;
 }
 
-
 interface IParam {
-    role: number,
-    index?: number
+    role: number;
+    idx?: number;
+    level: number;
 }
-
 
 interface IProps {
-    query: IParam,
-    params: object
+    query: IParam;
+    params: object;
 }
 
-
 const Title = (props: IProps) => {
-    const router = useRouter()
-    const { role } = props.query
-    const init = useQuery(GET_TITLE, {
-        variables: { role: Number(role) }
-    })
+    const router = useRouter();
+    const { role, idx, level } = props.query as IParam;
 
+    const bottomElement = useRef<HTMLDivElement>(null);
+
+    const [loading, setLoading] = useState(true);
     const [func, result] = useMutation(SET_TITLE);
 
     const [brief, setBrief] = useState<Array<IBrief>>([
@@ -162,19 +170,21 @@ const Title = (props: IProps) => {
     };
 
     const onClickNext = () => {
-        if(title.length > 0){
-            func({variables:
-                {
+        if (title.length > 0) {
+            func({
+                variables: {
                     service: {
+                        idx: Number(idx),
                         role: Number(role),
+                        level: Number(level),
                         title,
-                        brief_history : brief.filter(it => it.content).map(it => ({when : it.when, content: it.content}))
+                        brief_history: brief.filter(it => it.content).map(it => ({ when: it.when, content: it.content }))
                     }
                 }
-            })
-        }else{
-            setSnack(prev => ({...prev, message: '타이틀을 입력해주세요.', open:true}))
-            setTimeout(() => setSnack(prev => ({...prev, message: '', open:false})), 2000);
+            });
+        } else {
+            setSnack(prev => ({ ...prev, message: '타이틀을 입력해주세요.', open: true }));
+            setTimeout(() => setSnack(prev => ({ ...prev, message: '', open: false })), 2000);
         }
     };
 
@@ -187,45 +197,112 @@ const Title = (props: IProps) => {
     const { vertical, horizontal, open, message } = snack;
 
     useEffect(() => {
-        if(!role){
-            router.push('/login')
+        if (!role || !level) {
+            router.push('/login');
+        } else {
+            if (idx && !isNaN(idx)) {
+                const query = `
+                    query getTitle($idx: Int!) {
+                        getTitle(idx: $idx) {
+                            status
+                            data {
+                                idx
+                                title
+                                brief_history {
+                                    when
+                                    content
+                                }
+                            }
+                            token
+                            errors {
+                                code
+                                var
+                                text
+                            }
+                        }
+                    }`;
+                const SESSION: DocumentNode = gql`
+                    ${query}
+                `;
+                const innerQuery = SESSION.definitions[0] as OperationDefinitionNode;
+                const { value } = innerQuery.name as NameNode;
+
+                axiosApiInstance(value)
+                    .post(`${GQL_DOMAIN}`, {
+                        query: `${query}`,
+                        variables: {
+                            idx: Number(idx)
+                        }
+                    })
+                    .then(res => {
+                        setLoading(false);
+                        const { data } = res;
+                        const innerData = data[value]?.data;
+                        if (innerData) {
+                            setTitle(innerData.title);
+                            const briefHistory = innerData.brief_history as Array<IBrief>;
+                            if (briefHistory.length > 0) {
+                                setBrief(
+                                    briefHistory.map(it => {
+                                        let val = '';
+                                        if (it.when) {
+                                            const dateTime = new Date(it.when);
+                                            const month = dateTime.getMonth() + 1;
+                                            val = `${dateTime.getFullYear()}-${month < 10 ? `0${month}` : month}`;
+                                        }
+                                        return {
+                                            ...it,
+                                            value: val,
+                                            open: false
+                                        };
+                                    })
+                                );
+                            }
+                        }
+                    });
+            }
+            setLoading(false);
         }
-    }, [])
+
+        document.addEventListener('scroll', () => bottomEvent());
+
+        return () => {
+            document.removeEventListener('scroll', () => bottomEvent());
+        };
+    }, []);
+
+    const bottomEvent = () => {
+        if (window.innerHeight + window.scrollY + 100 > document.body.offsetHeight) {
+            if (bottomElement.current) {
+                bottomElement.current.style.position = 'initial';
+            }
+        }
+    };
 
     useEffect(() => {
-        if(result.data?.setTitle.status === 201){
-            const cookie = useCookie()
-            cookie.set(TOKEN, result.data.setTitle.token, { path: '/'})
-            onClickNext()
-        }else if(result.data?.setTitle.status === 200){
-
+        if (result.data?.setTitle.status === 201) {
+            const cookie = useCookie();
+            cookie.set(TOKEN, result.data.setTitle.token, { path: '/' });
+            onClickNext();
+        } else if (result.data?.setTitle.status === 200) {
+            router.push(`service/${result.data.setTitle.location}`);
         }
-    }, [result.data])
+    }, [result.data]);
 
-    if(init.loading){
-        return (
-            <Progress />
-        )
-    }
-
-    if(init.data?.getTitle.status === 201){
-        const cookie = useCookie()
-        cookie.set(TOKEN, init.data.getTitle.token, { path: '/'})
-        init.refetch()
-        return (
-            <Progress />
-        )
+    if (loading) {
+        return <Progress />;
     }
 
     return (
         <AbstractComponent>
+            <GlobalStyle />
             <TitleContainer>
                 <LocalizationProvider dateAdapter={AdapterDateFns} locale={koLocale}>
                     <HInput width="100%" label="타이틀" variant="outlined" name="id" value={title} onChange={onChangeTitle} />
                     <div style={{ position: 'relative', width: '100%', maxWidth: `${common.size.mobileWidth}px` }}>
                         {brief.map((item, index) => (
                             <div key={index} style={{ display: 'flex', justifyContent: 'space-between' }}>
-                                <div style={{ width: '30%', display: 'inline-block' }}>
+                                <div style={{ width: '35%', display: 'inline-block' }}>
                                     <DatePicker
                                         disableFuture
                                         label="년도"
@@ -263,7 +340,7 @@ const Title = (props: IProps) => {
                                     />
                                 </div>
                                 <HInput
-                                    width="65%"
+                                    width="60%"
                                     value={item.content}
                                     label={`약력 ${index + 1}`}
                                     variant="outlined"
@@ -278,8 +355,8 @@ const Title = (props: IProps) => {
                         </Fab>
                     </div>
                 </LocalizationProvider>
-                <div style={{ width: '100%', maxWidth: `${common.size.mobileWidth}px`, textAlign: 'right' }}>
-                    <HButton width="30%" onClick={onClickNext}>
+                <div ref={bottomElement} style={{ width: '100%', maxWidth: `${common.size.mobileWidth}px`, textAlign: 'right', position: 'fixed', bottom: '30px' }}>
+                    <HButton width="30%" onClick={onClickNext} style={{ marginTop: '30px' }}>
                         {result.loading ? <CircularProgress style={{ color: 'white' }} /> : '다음'}
                     </HButton>
                 </div>
